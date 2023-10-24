46f439e1c42375efeb9457a3f1ec79546dc47b57
OmaimaOuerghi <ouerghiomaima46@gmail.com>
2023-10-24 20:29:47 +0100

    redux

---
+++ todo/src/App.js
@@ -0,0 +1,20 @@
+import TaskList from "./component/Tasklist";
+import AddTask from "./component/addtask";
+
+import Main from "./component/main";
+
+function App() {
+  return (
+    <div className="App">
+       <Main/>
+      <div className="interface">
+      <AddTask/><br></br>
+      
+      <TaskList/><br></br>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
